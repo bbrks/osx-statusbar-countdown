091dc882e03db1fb631cfaaa7deaeee1a21a1aad
Ben Brooks <ben@bbrks.me>
2015-03-31 23:39:06 +0100

    Initial Xcode Project

---
+++ StatusbarCountdown/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  StatusbarCountdown
+//
+//  Created by Ben Brooks on 31/03/2015.
+//  Copyright (c) 2015 Ben Brooks. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
