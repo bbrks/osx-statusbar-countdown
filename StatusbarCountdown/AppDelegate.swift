ceca854319ff6c9ca7ab557cc5ba6aca9c59ea26
Ben Brooks <ben@bbrks.me>
2015-04-01 00:32:13 +0100

    Count down to hardcoded date in seconds

--- StatusbarCountdown/AppDelegate.swift
+++ StatusbarCountdown/AppDelegate.swift
@@ -11,14 +11,26 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     
+    let countToDate = NSDate(timeIntervalSince1970: 1431000000)
+
     let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
     @IBOutlet weak var statusMenu: NSMenu!
-    
+
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        
+
         statusItem.title = "Countdown"
         statusItem.menu = statusMenu
-        
+
+        // Run tick() once a second
+        NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "tick", userInfo: nil, repeats: true)
+
+    }
+
+    func tick() {
+        let now = NSDate()
+        let diff = Int(countToDate.timeIntervalSinceDate(now))
+
+        statusItem.title = String(diff)
     }
 
     @IBAction func quitApplication(sender: NSMenuItem) {
@@ -26,4 +38,3 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
 }
-
