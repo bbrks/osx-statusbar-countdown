4a5d4204b71ef8a5baa85aa082e87e4ec1818c10
Ben Brooks <ben@bbrks.me>
2015-04-01 01:41:11 +0100

    Add showName toggle

--- StatusbarCountdown/AppDelegate.swift
+++ StatusbarCountdown/AppDelegate.swift
@@ -11,6 +11,8 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     
+    var showName = true
+    
     let countToDate = NSDate(timeIntervalSince1970: 1431000000)
     let countdownName = "Diss"
 
@@ -19,7 +21,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
 
-        statusItem.title = "Countdown"
+        statusItem.title = ""
         statusItem.menu = statusMenu
 
         // Run tick() once a second
@@ -29,21 +31,57 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func tick() {
         let now = NSDate()
-        var diff = secondsToHoursMinutesSeconds(countToDate.timeIntervalSinceDate(now))
+        var diff = secondsToTime(Int(countToDate.timeIntervalSinceDate(now)))
+        
+        if (showName) {
+            statusItem.title = countdownName + ": " + formatTime(diff)
+        } else {
+            statusItem.title = formatTime(diff)
+        }
     
-        statusItem.title = countdownName + ": " + formatHMS(diff)
     }
     
-    func secondsToHoursMinutesSeconds (seconds : Double) -> (Double, Double, Double) {
-        let (hr,  minf) = modf (seconds / 3600)
-        let (min, secf) = modf (60 * minf)
-        return (hr, min, 60 * secf)
+    func secondsToTime (seconds : Int) -> (Int, Int, Int, Int, Int) {
+        
+        var forYears = seconds / (3600 * 24 * 365)
+        var remainder = seconds % (3600 * 24 * 365)
+        
+        var forDays = remainder / (3600 * 24)
+        remainder = remainder % (3600 * 24)
+        
+        var forHours = remainder / 3600
+        remainder = remainder % 3600
+        
+        var forMinutes = remainder / 60
+        var forSeconds = remainder % 60
+        
+        let s = forSeconds
+        let m = forMinutes
+        let h = forHours
+        let d = forDays
+        let y = forYears
+        return (y, d, h, m, s)
     }
     
-    func formatHMS(hmsTuple: (Double, Double, Double)) -> (String) {
-        return String(Int(hmsTuple.0)) + "h " + String(Int(hmsTuple.1)) + "m " + String(Int(hmsTuple.2)) + "s"
+    func formatTime(time: (Int, Int, Int, Int, Int)) -> (String) {
+        let y = (time.0 > 0) ? String(time.0) + "y " : ""
+        let d = (time.1 > 0 || time.0 > 0) ? String(time.1) + "d " : ""
+        let h = (time.2 > 0 || time.1 > 0 || time.0 > 0) ? String(time.2) + "h " : ""
+        let m = (time.3 > 0 || time.2 > 0 || time.1 > 0 || time.0 > 0) ? String(time.3) + "m " : ""
+        let s = String(time.4) + "s"
+        return y + d + h + m + s
     }
 
+    @IBAction func toggleShowName(sender: NSMenuItem) {
+        if (showName) {
+            showName = false
+            sender.state = NSOffState
+        } else {
+            showName = true
+            sender.state = NSOnState
+        }
+    }
+    
     @IBAction func quitApplication(sender: NSMenuItem) {
         NSApplication.sharedApplication().terminate(self);
     }
