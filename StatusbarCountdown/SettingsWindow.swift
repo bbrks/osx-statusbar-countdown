788a9ccdac53d7a9fb4ed3296641fd95d212795b
Jacqueline Alves <jacque.alves.b@gmail.com>
2019-10-02 08:23:03 -0300

    Create settings view

---
+++ StatusbarCountdown/SettingsWindow.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsWindow.swift
+//  StatusbarCountdown
+//
+//  Created by Jacqueline Alves on 02/10/19.
+//  Copyright © 2019 Ben Brooks. All rights reserved.
+//
+
+import Cocoa
+
+class SettingsWindow: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
